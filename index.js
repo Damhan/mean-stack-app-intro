b9e3bbedf46a799f9aab2f926813e36d2904cb58
Damhan Richardson <damhanrichardson@gmail.com>
2017-07-23 01:19:42 +0100

    Server-setup finished

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-const config = require('./config/database')
+const config = require('./config/database');
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -13,10 +14,13 @@ mongoose.connect(config.uri, (err) => {
 
 });
 
+app.use(express.static(__dirname + '/client/dist/'));
+
+
 //whenever get request is sent, we respond with "hello world"
 //the / is root/homepage
 app.get('*', function(req, res) {
-  res.send('<h1>Damhan</h1>');
+  res.sendfile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 //listen on the port 8080 which would be http://localhost:8080
